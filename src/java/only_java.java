1412ca65384f97a3ad01acf005d5dec6c825274e
CyberJ0 <mir.govna@mail.ru>
2017-12-17 20:40:22 +0200

    #1 test commit

---
+++ src/java/only_java.java
@@ -0,0 +1,11 @@
+package java;
+
+public class only_java {
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 64;
+        int sum = num1 + num2;
+        System.out.println(sum);
+
+    }
+}
